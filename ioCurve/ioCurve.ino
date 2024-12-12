28b7cfa3bb6808cab282b88290f01d09e60e2b24
LFP Ephys temp MINT PC <davidswygart@gmail.com>
2024-12-12 15:35:23 -0500

    new project for IO curve

---
+++ ioCurve/ioCurve.ino
@@ -0,0 +1,61 @@
+
+#include "parameters.h"
+
+// Pins
+const int startButtonPin = 2;
+const int outPin = 8; 
+const int outWidePin = 9;
+
+// State
+// bool shouldContinue = TRUE
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Starting setup");
+  pinMode(outPin, OUTPUT);
+  pinMode(outWidePin, OUTPUT);
+  pinMode(startButtonPin, INPUT);
+  loadSettings();
+  Serial.println("Setup finished");
+}
+
+void loop() {
+  if (buttonIsPressed()) {
+    Serial.println("Button pressed!");
+    Serial.println("---- Session 1 ----");
+    runStim(session1);
+  }
+}
+
+bool buttonIsPressed() {
+  int buttonState = digitalRead(startButtonPin);
+  return buttonState == HIGH;
+}
+
+void flexibleDelay(unsigned long t) {
+  if (t < 4000) {
+    delayMicroseconds(t);
+  } else {
+    delay(t/ms);
+  }
+}
+
+void runStim(Params params) {
+  for (int i=0; i<params.trainRepeats; i++){
+    Serial.print("Running Train #"); Serial.print(i+1); Serial.print("/");Serial.println(params.trainRepeats);
+    runTrain(params);
+    flexibleDelay(params.trainDelay);
+  }
+}
+
+void runTrain(Params params) {
+  for (int i=0; i<params.pulseRepeats; i++){
+    Serial.print("Running pulse #"); Serial.print(i+1); Serial.print("/");Serial.println(params.pulseRepeats);
+    PORTB = B00000011; // Turn on pin 8 and 9 (direct port manipulation)
+    flexibleDelay(params.pulseDur);
+    PORTB = B00000010; // turn off pin 8 (direct port manipulation)
+    flexibleDelay(params.widePulseDur-params.pulseDur);
+    PORTB = B00000000; // turn off pin 9 (direct port manipulation)
+    flexibleDelay(params.pulseDelay);
+  }
+}
---
+++ ioCurve/parameters.h
@@ -0,0 +1,26 @@
+//// All parameter times are in microseconds, but I've defined larger time units for your convenience.
+// Use them like this: session1.pulseDur = 1*ms;
+const unsigned long us = 1;
+const unsigned long ms = 1000;
+const unsigned long s = ms*1000;
+const unsigned long minute = s*60;
+
+//// Define a structure to hold session parameters and set default values ////
+struct Params {
+  unsigned long pulseDur = 200*us;
+  unsigned long pulseDelay = 10*s; 
+  byte pulseRepeats = 10;
+  unsigned long trainDelay = 60*s;
+  byte trainRepeats = 10;
+  unsigned long widePulseDur = 100*ms; //This needs to be equal or longer than pulseDur or you will get weird results
+};
+
+//// Create an instance for each session
+Params session1;
+
+
+////////////// Define custom parameters for each setting ///////////////////////////////
+void loadSettings(){
+
+  
+}
