206986a76b35cde6556d3739137467149ce8011f
Sohail Noor <msohailnoor@hotmail.com>
2026-06-25 19:57:47 -0400

    Added comments

--- pulseFriend/parameters.h
+++ pulseFriend/parameters.h
@@ -4,13 +4,12 @@
 const unsigned long us = 1;
 const unsigned long ms = 1000;
 const unsigned long s = ms * 1000;
-const unsigned long minute = s * 60;
 
 struct Params {
   unsigned long pulseDur = 60 * us;
   unsigned long interPhaseGap = 10 * us;
-  unsigned long pulsePeriod = 9870 * us;
-  unsigned long widePulseDur = 130 * us;
+  unsigned long pulsePeriod = 9870 * us; // 10 ms - 60 us - 10 us - 60 us
+  unsigned long widePulseDur = 130 * us; // ephys gate duration covering the full biphasic pair: 60 us + 10 us + 60 us
 
   unsigned int pulseRepeats = 1000;
   unsigned long trainDelay = 0 * ms;
